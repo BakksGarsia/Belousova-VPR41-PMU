bbfffdb449b6f7fb3cfbd7be2545501dc2b766c9
BakksGarsia <55590294+BakksGarsia@users.noreply.github.com>
2023-02-21 11:45:28 +0300

    LAB1

--- app/src/main/java/com/example/lr1/LogInPageActivity.java
+++ app/src/main/java/com/example/lr1/LogInPageActivity.java
@@ -5,18 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class LogInPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_log_in_page);
     }
 
-    public void startLogInPage(View view) {
-        Intent intent = new Intent(this, LogInPageActivity.class);
+    public void startSearch(View view) {
+        Intent intent = new Intent(this, SearchActivity.class);
         startActivity(intent);
     }
 }
