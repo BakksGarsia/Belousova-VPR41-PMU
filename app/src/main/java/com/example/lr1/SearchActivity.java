a5651b05f8197c09c00a160508b74ca7fcf4af08
BakksGarsia <55590294+BakksGarsia@users.noreply.github.com>
2023-02-21 13:08:33 +0300

    LAB1

--- app/src/main/java/com/example/lr1/SearchActivity.java
+++ app/src/main/java/com/example/lr1/SearchActivity.java
@@ -3,12 +3,26 @@ package com.example.lr1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class SearchActivity extends AppCompatActivity {
+import com.example.lr1.databinding.ActivitySearchBinding;
 
+public class SearchActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ActivitySearchBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        binding = ActivitySearchBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+}
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()== binding.btnSearch.getId()){
+
+
+        }
     }
 }
